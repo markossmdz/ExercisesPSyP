639d814fbca049ac89aaacae3506b7daa91ae7c2
Marcos <167062960+markossmdz@users.noreply.github.com>
2025-01-17 12:13:17 +0100

    Add files via upload
    Here it is the first exercise

---
+++ PSP1/src/psp_script/WashingCar.java
@@ -0,0 +1,24 @@
+package psp_script;
+
+public class WashingCar extends Thread {
+	private String carModel;
+	private int washingTime; //seconds
+
+	public WashingCar(String carModel, int washingTime) {
+		// TODO Auto-generated constructor stub
+		this.carModel = carModel;
+		this.washingTime = washingTime;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(carModel + " started its washing");
+		try {
+			Thread.sleep(washingTime * 1000L);
+		}catch(InterruptedException e){
+			System.out.println(carModel + " The owner ran out of coins");
+		}
+		System.out.println(carModel + " finished the washing");
+	}
+}
+		
